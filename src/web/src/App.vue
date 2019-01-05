f4587256ec0ae66a9b0794a018a5aad662930e6f
lubang <purebuddy@gmail.com>
2019-01-06 00:18:24 +0900

    Add dependencies and configure prettier
    - Typescript with no semicolon
    - Lint and formatting with prettier

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -6,8 +6,8 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from './components/HelloWorld.vue'
 
 @Component({
   components: {
--- src/web/src/main.ts
+++ src/web/src/main.ts
@@ -1,8 +1,14 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+import 'vuetify/dist/vuetify.min.css'
+
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 new Vue({
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount('#app')
