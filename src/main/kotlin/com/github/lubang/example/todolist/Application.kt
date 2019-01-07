591f5e7995ae3792da209de519bcacc9da2df6ee
lubang <purebuddy@gmail.com>
2019-01-07 11:52:54 +0900

    Configure H2 Web dashboard
    Add a property database.h2.web to display a dashboard.

--- src/main/kotlin/com/github/lubang/example/todolist/Application.kt
+++ src/main/kotlin/com/github/lubang/example/todolist/Application.kt
@@ -4,13 +4,14 @@ import com.github.lubang.example.todolist.port.adapter.web.KtorWebModule
 import com.google.inject.Guice
 import io.ktor.application.Application
 
+
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
     Guice.createInjector(
-        ApplicationModule(),
+        ApplicationModule(this),
         KtorWebModule(this)
     )
 }
--- src/main/kotlin/com/github/lubang/example/todolist/ApplicationModule.kt
+++ src/main/kotlin/com/github/lubang/example/todolist/ApplicationModule.kt
@@ -5,9 +5,12 @@ import com.github.lubang.example.todolist.port.adapter.persistent.ExposedTodoIte
 import com.google.inject.AbstractModule
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.ktor.application.Application
+import org.h2.tools.Server
 import org.jetbrains.exposed.sql.Database
+import java.util.concurrent.Executors
 
-class ApplicationModule : AbstractModule() {
+class ApplicationModule(private val application: Application) : AbstractModule() {
     init {
         initializeDatabase()
     }
@@ -22,6 +25,18 @@ class ApplicationModule : AbstractModule() {
         config.dataSourceProperties["url"] = "jdbc:h2:mem:todolist"
         config.validate()
 
-        Database.connect(HikariDataSource(config))
+        val database = Database.connect(HikariDataSource(config))
+        launchH2Web(database)
+    }
+
+    private fun launchH2Web(database: Database) {
+        @Suppress("EXPERIMENTAL_API_USAGE")
+        val h2Web = application.environment.config.propertyOrNull("database.h2.web")?.getString()
+            ?: "false"
+        if (h2Web.toBoolean()) {
+            Executors.newSingleThreadExecutor().submit {
+                Server.startWebServer(database.connector())
+            }
+        }
     }
 }
